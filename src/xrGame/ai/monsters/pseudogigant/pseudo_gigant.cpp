a8a9336340af83485a883555a2aa8d5e90f9be0d
OldSerpskiStalker <ictopiar@gmail.com>
2024-01-06 19:52:36 +0300

    Mutants do not touch quest items, polter, burer, pseudogigant

--- src/xrGame/ai/monsters/pseudogigant/pseudo_gigant.cpp
+++ src/xrGame/ai/monsters/pseudogigant/pseudo_gigant.cpp
@@ -243,12 +243,14 @@ void CPseudoGigant::on_activate_control(ControlCom::EControlType type)
 
 void CPseudoGigant::on_threaten_execute()
 {
-	// разбросить объекты
+	// СЂР°Р·Р±СЂРѕСЃРёС‚СЊ РѕР±СЉРµРєС‚С‹
 	m_nearest.clear();
 	Level().ObjectSpace.GetNearest	(m_nearest,Position(), 15.f, NULL); 
 	for (u32 i=0;i<m_nearest.size();i++) {
 		CPhysicsShellHolder  *obj = smart_cast<CPhysicsShellHolder *>(m_nearest[i]);
-		if (!obj || !obj->m_pPhysicsShell) continue;
+		if (!obj || !obj->m_pPhysicsShell || 
+			(pSettings->line_exist(obj->cNameSect().c_str(), "quest_item") && pSettings->r_bool(obj->cNameSect().c_str(), "quest_item"))) 
+			continue;
 
 		Fvector dir;
 		Fvector pos;
@@ -259,13 +261,13 @@ void CPseudoGigant::on_threaten_execute()
 		obj->m_pPhysicsShell->applyImpulse(dir,20 * obj->m_pPhysicsShell->getMass());
 	}
 
-	// играть звук
+	// РёРіСЂР°С‚СЊ Р·РІСѓРє
 	Fvector		pos;
 	pos.set		(Position());
 	pos.y		+= 0.1f;
 	m_sound_threaten_hit.play_at_pos(this,pos);
 
-	// играть партиклы
+	// РёРіСЂР°С‚СЊ РїР°СЂС‚РёРєР»С‹
 	PlayParticles(m_kick_particles, pos, Direction());
 	
 	CActor *pA = const_cast<CActor *>(smart_cast<const CActor *>(EnemyMan.get_enemy()));
@@ -277,11 +279,11 @@ void CPseudoGigant::on_threaten_execute()
 	hit_value		= m_kick_damage - m_kick_damage * dist_to_enemy / m_threaten_dist_max;
 	clamp			(hit_value,0.f,1.f);
 
-	// запустить эффектор
+	// Р·Р°РїСѓСЃС‚РёС‚СЊ СЌС„С„РµРєС‚РѕСЂ
 	Actor()->Cameras().AddCamEffector(xr_new<CMonsterEffectorHit>(m_threaten_effector.ce_time,m_threaten_effector.ce_amplitude * hit_value,m_threaten_effector.ce_period_number,m_threaten_effector.ce_power * hit_value));
 	Actor()->Cameras().AddPPEffector(xr_new<CMonsterEffector>(m_threaten_effector.ppi, m_threaten_effector.time, m_threaten_effector.time_attack, m_threaten_effector.time_release, hit_value));
 
-	// развернуть камеру
+	// СЂР°Р·РІРµСЂРЅСѓС‚СЊ РєР°РјРµСЂСѓ
 	if (pA->cam_Active()) {
 		pA->cam_Active()->Move(Random.randI(2) ? kRIGHT : kLEFT, Random.randF(0.3f * hit_value)); 
 		pA->cam_Active()->Move(Random.randI(2) ? kUP	: kDOWN, Random.randF(0.3f * hit_value)); 
@@ -289,7 +291,7 @@ void CPseudoGigant::on_threaten_execute()
 
 	Actor()->lock_accel_for	(m_time_kick_actor_slow_down);
 	
-	// Нанести хит
+	// РќР°РЅРµСЃС‚Рё С…РёС‚
 	NET_Packet	l_P;
 	SHit		HS;
 
